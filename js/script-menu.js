e77cf4e46a6031279ef6fb960425b1b8d21550d0
agathayun <RM561507@fiap.com.br>
2025-05-30 01:48:47 -0300

    arrumando o site sem responsividade, e começando responsividade

--- js/script-menu.js
+++
@@ -1,8 +0,0 @@
-const hamburguer = document.querySelector('.hamburguer');
-const menuOffScreen = document.querySelector('.menu-off-screen');
-
-
-hamburguer.addEventListener('click', () => {
-    hamburguer.classList.toggle('active');
-    menuOffScreen.classList.toggle('active');
-});
