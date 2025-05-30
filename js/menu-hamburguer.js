acded0687226d9b8719740b5f3c6d2854c025d61
agathayun <RM561507@fiap.com.br>
2025-05-30 20:44:42 -0300

    continuando responsividade, fazendo menu para celulares e finalizando pagina para desktops

--- js/menu-hamburguer.js
+++ js/menu-hamburguer.js
@@ -1,3 +1,6 @@
-function myFunction(x) {
-    x.classList.toggle("change")
-}
+const menuHam = document.querySelector('.menu-ham');
+const navOffScreen = document.querySelector('.nav-off-screen');
+menuHam.addEventListener('click', () => {
+    menuHam.classList.toggle('active');
+    navOffScreen.classList.toggle('active');
+});
